d8a292a694d4b571b7f36b403a316584e14505f4
Your Name <csomos.istvan@protonmail.com>
2023-01-29 14:43:32 +0100

    orai munka kesz

--- src/app.js
+++ src/app.js
@@ -3,3 +3,31 @@ import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import './scss/index.scss';
 import './index.html';
+import { signUp } from './auth.js';
+import renderPreLoaderPage from './pages/preloaderPage';
+import renderLoginPage from './pages/loginPage';
+import renderInnerPage from './pages/innerPage';
+import { async } from '@firebase/util';
+import { loginStatus } from './auth';
+import './eventlisteners';
+import { addData } from './database';
+
+// signUp('csomos.istvan@pm.me', '123456');
+
+// addData({ name: 'kutya', age: 3 });
+
+renderPreLoaderPage();
+
+async function main() {
+  try {
+    const user = await loginStatus();
+    if (user) {
+      console.log(user);
+      renderInnerPage();
+    }
+  } catch (error) {
+    renderLoginPage();
+    console.log(error);
+  }
+}
+main();
